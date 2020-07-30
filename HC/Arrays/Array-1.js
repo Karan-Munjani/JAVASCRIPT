42f2191de966a115ab05693a175172049534ce21
KaranMunjani <d_krn@users.noreply.github.com>
2020-07-30 16:38:00 +0530

    commit

--- HC/Arrays/Array-1.js
+++ HC/Arrays/Array-1.js
@@ -1,1 +1,29 @@
 let shopping = ['bread','milk','cheese','paneer'];
+
+console.log(shopping.length)
+
+
+
+// array from String
+
+let string = "hello there its Karan Munjani Here";
+
+let stringArray = string.split(" ");
+console.log(stringArray);
+console.log(stringArray[stringArray.length-1]);
+
+
+stringArray.push("!");
+stringArray.push("Okay.");
+stringArray.unshift("Listen!");
+stringArray.unshift("!");
+console.log(stringArray);
+
+stringArray.shift();
+
+console.log("\n", stringArray);
+console.log(stringArray.length);
+//string From array
+let string2 = stringArray.toString();
+
+console.log(string2)
--- HC/Arrays/Array-2.js
+++ HC/Arrays/Array-2.js
@@ -1,1 +1,19 @@
-var testArray
+let testArray = [2, 3, 4, 8, 5, 9, 1, 15]
+
+console.log(testArray.fill("0", 2, 6));
+
+//start index inclusive ending index exclusive
+console.log(testArray.fill("K", 1, 4));
+console.log(testArray.fill("K", 5));
+// will change entire ARRAY PASS BY REFRENCE
+
+
+let marks = [55, 80, 60, 72, 85, 90,91];
+
+let gradeA = marks.filter((mark) => mark >= 90);
+let gradeB = marks.filter((mark) => mark >= 70 && mark < 90);
+let gradeC = marks.filter((mark) => mark <50);
+
+console.log(gradeA);
+console.log(gradeB);
+console.log(gradeC);
